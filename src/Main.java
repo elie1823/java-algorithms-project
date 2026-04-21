169488faf4d2b413fa3aff9d7e3fcbc824a8397a
elie1823 <canelramahenina@gmail.com>
2026-04-21 09:34:15 +0300

    added sortingand Searching algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Arrays;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 9, 1, 3};
+
+        System.out.println("Avant tri : " + Arrays.toString(arr));
+
+        // 🔵 Test Bubble Sort
+        Sorting.bubbleSort(arr);
+        System.out.println("Après Bubble Sort : " + Arrays.toString(arr));
+
+        // 🟡 Test Selection Sort (nouveau tableau)
+        int[] arr2 = {7, 4, 8, 2, 1};
+        Sorting.selectionSort(arr2);
+        System.out.println("Selection Sort : " + Arrays.toString(arr2));
+
+        // 🟠 Test Insertion Sort
+        int[] arr3 = {10, 6, 3, 8, 2};
+        Sorting.insertionSort(arr3);
+        System.out.println("Insertion Sort : " + Arrays.toString(arr3));
+
+        // 🔍 Recherche
+        int index = Searching.linearSearch(arr, 3);
+        System.out.println("Linear Search (3) index: " + index);
+
+        int index2 = Searching.binarySearch(arr, 5);
+        System.out.println("Binary Search (5) index: " + index2);
     }
 }
